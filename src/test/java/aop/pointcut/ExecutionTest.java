7c73ccc98a7b99d9c8010242788fe282dee453c1
km1031kim@naver.com <km1031kim@naver.com>
2026-04-08 10:27:19 +0900

    포인트컷 지시자 : within

--- src/test/java/aop/pointcut/ExecutionTest.java
+++ src/test/java/aop/pointcut/ExecutionTest.java
@@ -4,6 +4,7 @@ import aop.member.MemberServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 
@@ -179,5 +180,17 @@ public class ExecutionTest {
         assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
     }
 
+    @Test
+    @DisplayName("타겟의 타입에만 직접 적용, 인터페이스를 선정하면 안된다.")
+    void withinSuperTypeFalse() {
+        pointcut.setExpression("within(aop.member.MemberService)");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isFalse();
+    }
 
+    @Test
+    @DisplayName("execution은 타입 기반, 인터페이스를 선정이 가능하다")
+    void executionSuperTypeTrue() {
+        pointcut.setExpression("execution(* aop.member.MemberService.*(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
 }
