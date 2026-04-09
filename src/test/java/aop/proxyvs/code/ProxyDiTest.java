4dc7df08588fedcc1f74476896c5394c95650fd5
km1031kim@naver.com <km1031kim@naver.com>
2026-04-09 18:10:27 +0900

    /**      * CGLIB 구체 클래스 기반 프록시 문제점      * 대상 클래스에 기본 생성자 필수      * 생성자 2번 호출 문제      * -> 타겟 클래스 생성 시, 프록시 상속 시 부모 생성자로 호출 -> 생성자 2번 호출..      * -> 스프링 4.0부터 objenesis 라는 특별한 라이브러리르 사용해서 기본 생성자 없이 객체 생성가능.      * final 키워드 클래스, 메서드 사용 불가      * -> 어차피 AOP 대상에는 이걸 잘 안쓴다.      *      * 결론 :  CGLIB이 디폴트다. 클라이언트 입장에서는 모르고 쓰면 된다.      */

--- src/test/java/aop/proxyvs/code/ProxyDiTest.java
+++ src/test/java/aop/proxyvs/code/ProxyDiTest.java
@@ -13,13 +13,14 @@ import org.springframework.context.annotation.Import;
  */
 @Slf4j
 @Import(ProxyDIAspect.class)
-@SpringBootTest(properties = {"spring.aop.proxy-target-class=false"}) // JDK 동적 프록시
+//@SpringBootTest(properties = {"spring.aop.proxy-target-class=false"}) // JDK 동적 프록시
+@SpringBootTest // JDK 동적 프록시
 public class ProxyDiTest {
 
     @Autowired
     MemberService memberService;
 
-    // JDK 동적 프록시는 구현 클래스로 타입 캐스팅 안됨
+        // JDK 동적 프록시는 구현 클래스로 타입 캐스팅 안됨
     @Autowired
     MemberServiceImpl memberServiceImpl;
 
@@ -29,4 +30,16 @@ public class ProxyDiTest {
         log.info("memberServiceImpl class={}", memberServiceImpl.getClass());
         memberServiceImpl.hello("hello");
     }
+
+    /**
+     * CGLIB 구체 클래스 기반 프록시 문제점
+     * 대상 클래스에 기본 생성자 필수
+     * 생성자 2번 호출 문제
+     * -> 타겟 클래스 생성 시, 프록시 상속 시 부모 생성자로 호출 -> 생성자 2번 호출..
+     * -> 스프링 4.0부터 objenesis 라는 특별한 라이브러리르 사용해서 기본 생성자 없이 객체 생성가능.
+     * final 키워드 클래스, 메서드 사용 불가
+     * -> 어차피 AOP 대상에는 이걸 잘 안쓴다.
+     *
+     * 결론 :  CGLIB이 디폴트다. 클라이언트 입장에서는 모르고 쓰면 된다.
+     */
 }
