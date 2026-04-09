26fc40a9f746456daa4f3aaea7f38b8d339a2244
km1031kim@naver.com <km1031kim@naver.com>
2026-04-09 10:52:26 +0900

    내부 호출 문제 대안3 : 구조 변경

---
+++ src/main/java/aop/internalcall/CallServiceV3.java
@@ -0,0 +1,25 @@
+package aop.internalcall;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * 내부 호출 문제 해결3 : 구조 변경
+ * 영한센세와 스프링이 권장하는 방법
+ */
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CallServiceV3 {
+
+    private final InternalService internalService;
+
+    public void external() {
+        log.info("call external");
+        internalService.internal();
+    }
+}
