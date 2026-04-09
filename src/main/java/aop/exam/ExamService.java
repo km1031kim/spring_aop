647f2d4f785843c1925bd0df6dff0c00bbded305
km1031kim@naver.com <km1031kim@naver.com>
2026-04-09 09:49:38 +0900

    Log AOP

---
+++ src/main/java/aop/exam/ExamService.java
@@ -0,0 +1,18 @@
+package aop.exam;
+
+import aop.exam.annotation.Trace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ExamService {
+
+    private final ExamRepository repository;
+
+    @Trace
+    public void request(String itemId) {
+        repository.save(itemId);
+    }
+}
+
